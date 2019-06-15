20ea92ab9eff2f70cc1df70e5e08d721addeef91
allys-m <51463868+allys-m@users.noreply.github.com>
2019-06-15 13:38:38 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,13 +1,19 @@
 let user = {};
 
 user.agent = [
-  navigator.appName,
-  navigator.appCodeName,
+  window.navigator.appName,
+  window.navigator.appCodeName,
+  window.navigator.userAgent,
+  window.navigator.cookieEnabled,
+  navigator.platform,
   window.location.protocol,
-  window.location.host,
+  window.location.host
   
 ];
 
 const mapp = ()=>{
-   alert('User.nav.appName '+user.agent[1]);
+   alert('User.nav.appName '+user.agent[0]);
+   for (let i=0; i<=user.agent.length; i++){
+      document.write(i+' '+user.agent[i]+'<br>');
+   }
 };
